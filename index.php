ab7550ab8747cfc3bcda241d68c967489dc4ec97
kawaldevops <kawaljeet.ethans@gmail.com>
2025-09-10 21:44:00 +0530

    new files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Web Page</title>
+</head>
+<body>
+    <h1>Hello from PHP!</h1>
+    <p>Current server time is: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
